f4d31619ec731aa8561ece6cff67c4a865e13ce1
Eli <amponsaeli@gmail.com>
2018-03-05 16:24:50 -0500

    new

--- master.js
+++ master.js
@@ -1,37 +1,8 @@
-$(function() {
-    var html = $('html, body'),
-        navContainer = $('.nav-container'),
-        navToggle = $('.nav-toggle'),
-        navDropdownToggle = $('.has-dropdown');
-
-    // Nav toggle
-    navToggle.on('click', function(e) {
-        var $this = $(this);
-        e.preventDefault();
-        $this.toggleClass('is-active');
-        navContainer.toggleClass('is-visible');
-        html.toggleClass('nav-open');
-    });
-
-    // Nav dropdown toggle
-    navDropdownToggle.on('click', function() {
-        var $this = $(this);
-        $this.toggleClass('is-active').children('ul').toggleClass('is-visible');
-    });
-
-    // Prevent click events from firing on children of navDropdownToggle
-    navDropdownToggle.on('click', '*', function(e) {
-        e.stopPropagation();
-    });
-});
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-36251023-1']);
-  _gaq.push(['_setDomainName', 'jqueryscript.net']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
